5211f0a6640af257a39c912633ee6b20ba5cf8fb
Ivan Dorosevich <loki.mail.com@gmail.com>
2020-11-13 09:53:47 +0300

    add fixed project

---
+++ src/main/java/com/github/loki16v/helloci/App.java
@@ -0,0 +1,13 @@
+package com.github.loki16v.helloci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
